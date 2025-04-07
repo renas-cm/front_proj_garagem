22e58740033349ad18b2bf02ad833c7d1a85f7cb
Marco André Mendes <marcoandre@gmail.com>
2025-04-07 15:33:08 -0300

    feat: add vite-plugin-vue-devtools for enhanced Vue debugging support

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { fileURLToPath, URL } from 'url'
 
 import { defineConfig } from 'vite'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
